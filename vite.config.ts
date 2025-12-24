d038f01247f3a1903c935a946f801c3d55ff9e06
bilal <bilaldeveloper900@gmail.com>
2025-12-24 21:15:32 +0500

    back

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
     build: {
-      outDir: 'dist',
+      outDir: 'build/client',
     },
 });
